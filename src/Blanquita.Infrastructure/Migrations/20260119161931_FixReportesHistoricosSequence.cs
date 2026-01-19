7df7d70c8567e44ec8740a304df4d4e9faf7ebc9
Alexis Barrón <alex.osber@gmail.com>
2026-01-19 10:36:17 -0600

    Fix(DB): Reset ReportesHistoricos sequence to avoid duplicate PK errors

---
+++ src/Blanquita.Infrastructure/Migrations/20260119161931_FixReportesHistoricosSequence.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blanquita.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixReportesHistoricosSequence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SELECT setval(pg_get_serial_sequence('\"ReportesHistoricos\"', 'Id'), COALESCE((SELECT MAX(\"Id\") FROM \"ReportesHistoricos\"), 0));");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
